ff5970f4ff053059c74125a8ce8d32d55ac3791b
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-17 16:25:42 +0000

    small changes made

--- src/MegaByteConv.java
+++ src/MegaByteConv.java
@@ -1,9 +1,4 @@
 public class MegaByteConv {
-    public static void main(String[] args) {
-        printMegaBytesAndKiloBytes(2500);
-        printMegaBytesAndKiloBytes(-1024);
-        printMegaBytesAndKiloBytes(5000);
-    }
     public static void printMegaBytesAndKiloBytes( int kilobytes){
         int megabytes;
         int remainingKilobytes;
@@ -13,7 +8,8 @@ public class MegaByteConv {
             System.out.println("Invalid Value");
         }
         else {
-            System.out.println(kilobytes+"KB = "+megabytes+"MB and "+remainingKilobytes+"KB");
+            System.out.println(kilobytes+" KB = "+megabytes+" MB and "+remainingKilobytes+" KB");
         }
     }
 }
+//Victor.Aremu
